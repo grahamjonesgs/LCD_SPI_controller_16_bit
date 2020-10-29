4afb1d030e66ac4d60f9fa4548ee95ff7b3fda3c
Graham <grahamjonesgs@gmail.com>
2020-10-29 21:30:45 +0100

    Fix to stack controller

--- LCD_SPI_controller_16_bit.runs/synth_1/LCD_SPI_controller_16_bit.tcl
+++ LCD_SPI_controller_16_bit.runs/synth_1/LCD_SPI_controller_16_bit.tcl
@@ -73,6 +73,7 @@ OPTRACE "synth_1" START { ROLLUP_AUTO }
 set_param tcl.collectionResultDisplayLimit 0
 set_param xicom.use_bs_reader 1
 set_param chipscope.maxJobs 1
+set_msg_config -id {Common 17-41} -limit 10000000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a35tcpg236-1
 
--- LCD_SPI_controller_16_bit.srcs/sources_1/new/stack.v
+++ LCD_SPI_controller_16_bit.srcs/sources_1/new/stack.v
@@ -13,11 +13,30 @@ output reg o_stack_error;
 
 (* ram_style = "block" *) reg [15:0] RAM [1023:0];
 reg [15:0] o_stacktop;
-reg [15:0] r_SP;
+reg [15:0] r_stack_pointer;
+
+   ila_0  myila2(.clk(clk),
+   .probe0(o_stacktop),
+   .probe1(r_stack_pointer),
+   .probe2(r_PC),
+   .probe3(i_write_value),
+   .probe4(o_stack_top_value),
+   .probe5(1'b0),
+   .probe6(1'b0),
+   .probe7(1'b0),
+   .probe8(1'b0),
+   .probe9(1'b0),
+   .probe10(1'b0),
+   .probe11(1'b0),
+   .probe12(1'b0),
+   .probe13(o_stack_error),
+   .probe14(i_write_flag),
+   .probe15(i_read_flag));
+
 
 initial
 begin
-    r_SP=15'h0;
+    r_stack_pointer=15'h0;
     o_stack_error=1'b0;
 end
 
@@ -25,40 +44,40 @@ always @(posedge clk)
 begin
   if (i_reset)
   begin
-    r_SP<=15'h0;
+    r_stack_pointer<=15'h0;
     o_stack_error<=1'b0;
   end //if (i_reset)
   else
   begin
     if (i_read_flag)
     begin
-      if (r_SP==0)
+      if (r_stack_pointer==0)
        begin
         o_stack_error<=1;  
-      end // if(r_SP==0)
+      end // if(r_stack_pointer==0)
       else
       begin
-         r_SP<=r_SP-1; 
-      end // else if if(r_SP==0)    
+         r_stack_pointer<=r_stack_pointer-1; 
+      end // else if if(r_stack_pointer==0)    
     end // if (i_read)
     if (i_write_flag)
     begin
-      if (r_SP>1022)
+      if (r_stack_pointer>1022)
       begin
          o_stack_error<=1; 
-      end // if (r_SP>1022)
+      end // if (r_stack_pointer>1022)
       else
       begin
-          RAM[r_SP]<=i_write_value;
-          r_SP<=r_SP+1;
-      end // else if (r_SP>1022)
+          RAM[r_stack_pointer]<=i_write_value;
+          r_stack_pointer<=r_stack_pointer+1;
+      end // else if (r_stack_pointer>1022)
     end //if (i_write_flag)
   end // else if (i_reset)
 end // always clock
 
 always @(posedge clk)
 begin
-   o_stack_top_value<=RAM[r_SP-1];
+   o_stack_top_value<=RAM[r_stack_pointer-1];
 end
 
 endmodule
